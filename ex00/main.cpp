a0b8a8377eb91e98006010247e4ac2bfd470a71a
jinholee <jinholee@student.42seoul.kr>
2023-02-23 20:22:40 +0900

    updated main.cpp"

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jinholee <jinholee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:44:51 by jinholee          #+#    #+#             */
-/*   Updated: 2023/02/07 16:16:18 by jinholee         ###   ########.fr       */
+/*   Updated: 2023/02/23 20:21:07 by jinholee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	main(void)
 	ClapTrap C = ClapTrap(B);
 	ClapTrap D = A;
 
-	std::cout << A.getName() << std::endl;
-	std::cout << B.getName() << std::endl;
-	std::cout << C.getName() << std::endl;
-	std::cout << D.getName() << std::endl;
+	std::cout << "A getName(): " << A.getName() << std::endl;
+	std::cout << "B getName(): " << B.getName() << std::endl;
+	std::cout << "C getName(): " << C.getName() << std::endl;
+	std::cout << "D getName(): " << D.getName() << std::endl;
 	A.attack(B.getName());
 	B.takeDamage(A.getAttackDamage());
 	B.beRepaired(1);
-	std::cout << B.getEnergyPoints() << std::endl;
-	std::cout << B.getHitPoints() << std::endl;
+	std::cout << "B getEnergyPoints(): " << B.getEnergyPoints() << std::endl;
+	std::cout << "B getHitPoints(): " << B.getHitPoints() << std::endl;
 	return (0);
 }

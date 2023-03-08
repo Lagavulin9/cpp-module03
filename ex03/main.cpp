c58e5da19f477d009c59bd17373a62c457c5520e
jinholee <jinholee@student.42seoul.kr>
2023-03-08 13:20:39 +0900

    main change

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jinholee <jinholee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:17:20 by jinholee          #+#    #+#             */
-/*   Updated: 2023/03/01 16:32:21 by jinholee         ###   ########.fr       */
+/*   Updated: 2023/03/08 13:20:19 by jinholee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int	main(void)
 	std::cout << "Hitpoint: " << A.getHitPoints() << std::endl;
 	std::cout << "EnergyPoint: " << A.getEnergyPoints() << std::endl;
 	std::cout << "AttackDamage: " << A.getAttackDamage() << std::endl;
-	A.attack("Hiroi");
+	A.attack("Some random trap");
 	return (0);
 }
